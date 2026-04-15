8dee858822d52c5a2fef55438a4eae0aa9b45f63
davirodrialencar-web <davirodrialencar@gmail.com>
2026-04-15 16:23:10 -0300

    TESTES-JAVA-SCRIPT
    TESTES NO JAVA SCRIPT

---
+++ teste2.js
@@ -0,0 +1,3 @@
+let exemplo1 = "1"
+let exemplo2 = 1
+console.log(exemplo1 + exemplo2)
---
+++ teste3.js
@@ -0,0 +1,4 @@
+let notaDoAluno1 = 7.4
+console.log("a nota do aluno foi notaDoAluno1")
+console.log(`a nota do aluno foi de notaDoAluno1`)
+
